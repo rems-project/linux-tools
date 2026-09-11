bd7422542334d11cb38c72301bcf2527973a18f7
Kayvan Memarian <kayvan.memarian@cl.cam.ac.uk>
2026-09-11 02:28:56 +0100

    adding linux kmake and making config multi platform

--- config.sh
+++ config.sh
@@ -1,8 +1,23 @@
-# using the system clang
-# CLANG=/usr/bin/clang
+die() {
+  print -u2 -- "\x1b[1;31mERROR\x1b[22;31m: $*\x1b[0m"
+  exit 1
+}
 
 # using the homebrew clang
 # CLANG=/opt/homebrew/opt/llvm@12/bin/clang-12
-CONFIG_CLANG=/opt/homebrew/opt/llvm/bin/clang
+if [[ $OSTYPE == darwin* && -f /opt/homebrew/opt/llvm/bin/clang ]]; then
+  # if on macos, use the homebrew clang when present
+  CONFIG_CLANG=/opt/homebrew/opt/llvm/bin/clang
+elif (( $+commands[clang] )); then
+  CONFIG_CLANG="$commands[clang]"
+else
+  die "no suitable clang was found"
+fi
 
-CONFIG_CROSS_COMPILE_PREFIX=aarch64-none-elf-
+if [[ $OSTYPE == linux* && $CPUTYPE == aarch64 ]]; then
+  CONFIG_CROSS_COMPILE_PREFIX=""
+elif [[ $$OSTYPE == darwin* ]]; then
+  CONFIG_CROSS_COMPILE_PREFIX=aarch64-none-elf-
+else
+  die "cross compile prefix needed"
+fi
